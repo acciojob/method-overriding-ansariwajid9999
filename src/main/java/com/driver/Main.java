212f485bee5f4c2e67ce5e6c7a3fa5dca887fbb5
Wajid <ansariwajid9999@gmail.com>
2023-07-12 18:14:19 +0530

    code changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,35 @@
 package com.driver;
 import java.sql.SQLOutput;
 import java.util.spi.AbstractResourceBundleProvider;
-public class Main {
 
+public class Main
+{
     public static class A{
-        public String meth() {
+        public A(){}
+
+        public String meth()
+        {
             return "Invoking method from class A";
         }
     }
-    public static class B extends A{
-        public String meth() {
-            return "Method is overridden is Extendend class B";
+
+    public static  class B extends A
+    {
+
+        public String meth()
+        {
+
+            return "Method is overridden in Extendend class B";
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[]args)
+    {
         B obj=new B();
         obj.meth();
 
         B obj1=new B();
         obj1.meth();
+
     }
 }
